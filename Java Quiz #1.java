20db2fa6520b82a18b6bb663e792dff20fd9fa42
SAC-CS112-Vu-Kiefer <kiefervu@gmail.com>
2017-04-26 19:21:06 -0700

    Add files via upload

---
+++ Java Quiz #1.java
@@ -0,0 +1,14 @@
+import java.util.student;
+
+public class Student {
+	s1 student = (s1) new student("Kiefer", "Vu");
+
+	public static void main(String[] args) {
+
+		String firstname = "Kiefer ";
+		String lastname = "Vu. ";
+		System.out.println("Student is " + firstname + lastname);
+
+	}
+
+}
