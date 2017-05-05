4fb8c20f047fc97066f8ea259fd6db158a93cebe
SAC-CS112-Vu-Kiefer <kiefervu@gmail.com>
2017-05-05 11:37:17 -0700

    Add files via upload

---
+++ car.java
@@ -0,0 +1,17 @@
+
+public class car {
+
+	public static void Honk() {
+		System.out.println("Beep Beeep Beeep!");
+
+	}
+
+	public static void OvertheToString() {
+	}
+
+	public void Displaymessage() {
+		System.out.println("Honk!");
+
+	}
+
+}
