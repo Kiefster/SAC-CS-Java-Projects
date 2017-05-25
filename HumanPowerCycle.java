ae93b43b19d2f0bfc1e74ef0c8752dcabfc1b800
SAC-CS112-Vu-Kiefer <kiefervu@gmail.com>
2017-05-24 21:39:01 -0700

    Add files via upload

---
+++ HumanPowerCycle.java
@@ -0,0 +1,32 @@
+public class HumanPowerCycle {
+	// member of instance variables
+	int NumberofWheels = 0;
+	int NumberofGears = 0;
+	String name = "";
+
+	// constructor that takes in the amount of wheels.
+	public int Wheel() {
+		return NumberofWheels;
+	}
+
+	// method setting the number of gears.
+	public int Gears(int num) {
+		return NumberofGears = num;
+	}
+
+	public void SetCurrentGears(double NumberofGears) {
+		double num = 0;
+		NumberofGears = num;
+
+	}
+
+	public String color(String color) {
+		return this.color(color);
+	}
+
+	public String name(String name) {
+		return name;
+
+	}
+
+}
